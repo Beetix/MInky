c889bc2024b82cf30142063360c65ecd04364f07
Benjamin Freeman <b.t.freeman16@gmail.com>
2022-04-28 21:04:09 +0200

    Add presentation controls

--- include/icons.h
+++ include/icons.h
@@ -1,6 +1,7 @@
 #ifndef ICONS_H
 #define ICONS_H
 
-extern unsigned char speaker_icon[5000];
+extern const unsigned char speaker_icon[5000];
+extern const unsigned char dashboard_icon[5000];
 
 #endif
--- src/MInky.ino
+++ src/MInky.ino
@@ -2,38 +2,81 @@
 #include <BleKeyboard.h>
 #include "icons.h"
 
+typedef enum {
+  MODE_MEDIA,
+  MODE_PRESENTATION,
+  MODE_MAX
+} minky_mode_t;
+
 BleKeyboard bleKeyboard("MInky", "Beetix");
 
 void setup()
 {
   M5.begin();
   digitalWrite(LED_EXT_PIN,LOW);
-  M5.M5Ink.clear();
-  M5.M5Ink.drawBuff((uint8_t *)speaker_icon);
   bleKeyboard.begin();
 }
 
 void loop()
 {
+  static minky_mode_t mode = MODE_MEDIA;
+
   M5.update();
   if( M5.BtnPWR.wasPressed())
   {
-      digitalWrite(LED_EXT_PIN,LOW);
-      M5.shutdown();
+    digitalWrite(LED_EXT_PIN,LOW);
+    M5.shutdown();
+  }
+  if( M5.BtnEXT.wasPressed())
+  {
+    mode = (minky_mode_t) (((int) mode + 1) % MODE_MAX);
+    M5.M5Ink.clear();
+    switch(mode)
+    {
+      case MODE_MEDIA:
+        M5.M5Ink.drawBuff((uint8_t *)speaker_icon);
+        break;
+      case MODE_PRESENTATION:
+        M5.M5Ink.drawBuff((uint8_t *)dashboard_icon);
+        break;
+    }
   }
   if (bleKeyboard.isConnected())
   {
     if( M5.BtnDOWN.wasPressed() )
     {
-      bleKeyboard.write(KEY_MEDIA_VOLUME_DOWN);
+      switch(mode)
+      {
+        case MODE_MEDIA:
+          bleKeyboard.write(KEY_MEDIA_VOLUME_DOWN);
+          break;
+        case MODE_PRESENTATION:
+          bleKeyboard.write(KEY_DOWN_ARROW);
+          break;
+      }
     }
     if(M5.BtnUP.wasPressed())
     {
-      bleKeyboard.write(KEY_MEDIA_VOLUME_UP);
+      switch(mode)
+      {
+        case MODE_MEDIA:
+          bleKeyboard.write(KEY_MEDIA_VOLUME_UP);
+          break;
+        case MODE_PRESENTATION:
+          bleKeyboard.write(KEY_UP_ARROW);
+          break;
+      }
     }
     if (M5.BtnMID.wasPressed())
     {
-      bleKeyboard.write(KEY_MEDIA_PLAY_PAUSE);
+      switch(mode)
+      {
+        case MODE_MEDIA:
+          bleKeyboard.write(KEY_MEDIA_PLAY_PAUSE);
+          break;
+        case MODE_PRESENTATION:
+          break;
+      }
     }
   }
   delay(10);
